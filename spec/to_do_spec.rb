81521d1f9f66dc97a35ed1b8cf835359b0eba6f2
Aaron Nguyen and Peter Karellas <peter@>
2017-04-25 09:38:58 -0700

    final

---
+++ spec/to_do_spec.rb
@@ -0,0 +1,37 @@
+require('rspec')
+require('task')
+
+
+describe(Task) do
+  before() do
+    Task.clear()
+  end
+end
+
+describe(Task) do
+  describe("#description") do
+    it("lets you give it a description")do
+      test_task= Task.new("scrub the zebra")
+      expect(test_task.description()).to(eq("scrub the zebra"))
+    end
+  end
+    describe(".all")do
+      it("is empty at first") do
+        expect(Task.all()).to(eq([]))
+    end
+  end
+  describe("#save") do
+    it("adds a task to the array of saved tasks") do
+      test_task= Task.new("wash the lion")
+      test_task.save()
+      expect(Task.all()).to(eq([test_task]))
+    end
+  end
+  describe(".clear")do
+    it("empties out all of the saved tasks") do
+      Task.new("wash the lion").save()
+      Task.clear()
+      expect(Task.all()).to(eq([]))
+    end
+  end
+end
