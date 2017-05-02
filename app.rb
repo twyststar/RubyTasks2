f845117e27a13d1f176b0f884f93f36c7fc6a146
Peter Karellas and Starlynne Start <Guest@epicodus-88.local>
2017-05-02 14:08:02 -0700

    Add integration specs and methods for single list, tasks.

--- app.rb
+++ app.rb
@@ -6,16 +6,42 @@ require('./lib/list')
 require('pry')
 require('pg')
 
-DB = PG.connect({:dbname => "to_do"})
+DB = PG.connect({:dbname => "to_do_test"})
 
 get('/') do
   @tasks = Task.all()
   erb(:index)
 end
 
-post("/tasks")do
-  description= params.fetch("description")
-  task = Task.new(description)
-  task.save()
-  erb(:results)
+get("/lists/new") do
+  erb(:list_form)
 end
+
+post("/lists") do
+  name = params.fetch("name")
+  list = List.new({:name => name, :id => nil})
+  list.save()
+  erb(:success)
+ end
+ get('/lists') do
+  @lists = List.all()
+  erb(:lists)
+end
+get("/lists/:id") do
+ @list = List.find(params.fetch("id").to_i())
+ erb(:list)
+end
+post("/tasks") do
+  description = params.fetch("description")
+  list_id = params.fetch("list_id").to_i()
+  @list = List.find(list_id)
+  @task = Task.new({:description => description, :list_id => list_id})
+  @task.save()
+  erb(:success)
+end
+# post("/tasks")do
+#   description= params.fetch("description")
+#   task = Task.new(description)
+#   task.save()
+#   erb(:results)
+# end
